8b07aabe9c5d5846161cf1cffb8c15e3f0c33d3c
Ismakii <ismaelglaydson46@gmail.com>
2024-11-27 19:18:19 -0300

    Alteração feita pelo pc

--- projetos/calcula.js
+++ projetos/calcula.js
@@ -1,10 +1,20 @@
 function calidade() {
   var masc = document.getElementById("masculino");
   var femi = document.getElementById("femenino");
+  var nascimento = document.getElementById("nasceu").value
+  var nasceu = Number(nascimento)
   var res = document.getElementById("resposta");
   if (masc.checked) {
-    res.innerHTML = `masculino`;
-  } else if (femi.checked) {res.innerHTML = `feminino`;}
+    const agora = new Date();
+    const ano = agora.getFullYear()
+    const anoatual = Number(ano)
+    idade = anoatual - nasceu
+    res.innerHTML = `Você é um jovem de ${idade} anos`;
+    if (idade < 20 && idade >= 14)
+      res.innerHTML = `dfsjdbfisdkh`;
+  } else if (femi.checked) {
+    res.innerHTML = `feminino`;
+  }
   else {
     res.innerHTML = 'Preencha todos os campos'
   }
