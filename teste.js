986d0b8806c9919d775290214b197cb8d921b33f
Ismakii <ismaelglaydson46@gmail.com>
2024-12-04 13:59:00 -0300

    Finalizado o exercicio de contador com passos.

---
+++ teste.js
@@ -0,0 +1,12 @@
+inicial = 1
+fim = 10
+passo = 2
+/*
+while(inicial <= fim){
+    console.log("Variável "+ inicial)
+    inicial += passo
+}*/
+
+for(inicial; inicial <= fim; inicial += passo){
+    console.log("Variavel "+ inicial)
+}
